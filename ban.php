385c0950c02ab7c807d7793bfe631d6fb1c7ccb9
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2020-12-02 14:29:24 +0900

    Update ban.php

--- ban.php
+++ ban.php
@@ -16,11 +16,9 @@ echo "no-username";
 exit;
 }
 
-if(!isset($_POST['IP']) || $_POST['IP']!= "official-bot"){
+
 $ip = $_SERVER["REMOTE_ADDR"];
-}else{
-$ip = $_POST['IP'];
-}
+
 $db['host'] = 'localhost';
 $db['user'] = 'root';
 $db['pass'] = '';
@@ -29,13 +27,7 @@ $db['dbname'] = 'gbans';
 $dsn = sprintf('mysql: host=%s; dbname=%s; charset=utf8', $db['host'], $db['dbname']);
 $pdo = new PDO($dsn, $db['user'], $db['pass'], array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
 
-$stmt = $pdo->prepare("SELECT * FROM gbanuban WHERE name = ?");
-$stmt->execute(array($_POST['user']));
-$result = $stmt->fetch(PDO::FETCH_ASSOC);
-if(count($result) > 0&& is_array($result)){
-echo "Access denied (user).";
-exit;
-}
+
 
 $stmt = $pdo->prepare("SELECT * FROM gbanipban WHERE name = ?");
 $stmt->execute(array($ip));
@@ -44,16 +36,8 @@ if(count($result) > 0&& is_array($result)){
 echo "Access denied (ip).";
 exit;
 }
-if(isset($_POST['server'])){
-$stmt = $pdo->prepare("SELECT * FROM gbanserverban WHERE name = ?");
-$stmt->execute(array($_POST['server']));
-$result = $stmt->fetch(PDO::FETCH_ASSOC);
-if(count($result) > 0&& is_array($result)){
-echo "Access denied (server).";
-exit;
-}
-}
-/*
+
+
 $stmt = $pdo->prepare("SELECT * FROM gbanchecklist WHERE name = ? AND ip = ?");
 $stmt->execute(array($_POST['username'],$ip));
 $result = $stmt->fetch(PDO::FETCH_ASSOC);
@@ -61,5 +45,5 @@ if(count($result) == 0 || !is_array($result)){
 echo "You have never checked this user.";
 exit;
 }
-*/
+
 require_once("./dosblock.php");
